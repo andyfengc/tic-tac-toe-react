e2adb9075671964e1fdf88b6bb27c3ac9783ee28
Owner <andyfengc@gmail.com>
2025-04-26 23:06:01 -0400

    add array; switch x/o

--- src/App2.js
+++ src/App2.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export default function App2() {
     return (
         <Board></Board>
@@ -5,30 +7,44 @@ export default function App2() {
 }
 
 export function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
     return (<>
         <div className="board-row">
-            <Square value={1}/>
-            <Square value={2}/>
-            <Square />
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
     </>
     )
-}
-export function Square({value}) {
-    function handleClick(){
-        console.log('clicked')
+
+    function handleClick(i) {
+        if (squares[i]) {return;}
+        const nextSquares = squares.slice();
+        if (xIsNext){
+            nextSquares[i] = "X";
+        }
+        else{
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
+}
+
+export function Square({ value, onSquareClick }) {
     return (
-        <button className="square" onClick={handleClick}>{value}</button>
+        <button className="square" onClick={onSquareClick}>{value}</button>
     )
 }
